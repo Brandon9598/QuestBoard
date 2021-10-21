76973755c4eee3a063d8d77400e41f9d255f7add
ludafieno <ludafieno@outlook.com>
2021-10-20 22:58:44 -0400

    handle auth login and logout with navbar; one navbar page

---
+++ src/Components/QuestNavbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { auth, signInWithGoogle, logout } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function QuestNavbar() {
+    const [user, loading, error] = useAuthState(auth); //remembers user 
+
+    return (
+        <div>            
+            <Navbar bg="primary" fixed="top" variant="dark" expand="md">
+                <Container>
+                    <Navbar.Brand>
+                        QuestBoard
+                    </Navbar.Brand>
+
+                    <Nav>
+                        {user ? (
+                            <>
+                            <Nav.Link>Pending Quests</Nav.Link>
+                            <Nav.Link>The Board</Nav.Link>
+                            <Nav.Link>My Account</Nav.Link>
+                            <Button variant="light" onClick={logout}>Sign Out</Button>
+                            </>
+
+                        ) : (
+                            <>
+                            <Nav.Link>The Board</Nav.Link>
+                            <Button variant="light" onClick={signInWithGoogle}>Login</Button>
+                            </>
+                        )}
+                        
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default QuestNavbar;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Home.css";
-import NavbarDefault from '../Components/NavbarDefault';
+import QuestNavbar from '../Components/QuestNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { signInWithGoogle } from "../firebase";
 
@@ -9,7 +9,7 @@ function Home() {
     return (
     <div>
         
-        <NavbarDefault /> 
+        <QuestNavbar /> 
 
         <div className="bg-image">
             <div>
@@ -68,8 +68,7 @@ function Home() {
                                 <p class="card-text text-sm-left">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sunt quasi voluptate ullam a dicta quidem repudiandae enim, quae, error, aliquid cupiditate pariatur libero! Dolores provident quas eos assumenda nostrum excepturi nobis sequi perspiciatis neque rem aliquid eveniet quibusdam dolor rerum repellendus, reiciendis, alias esse modi voluptas, mollitia suscipit consequatur!
                                 </p>
-                                {/* TODO: MAKE THIS WORK LIKE SIGNIN ABOVE */}
-                                <a href="#" class="btn btn-success">Create Account</a>
+                                <button onClick={signInWithGoogle} className="btn btn-success">Create Account</button>
                             </div>
                         </div>
                     </div>
