1c71a8cee021ab63c622f66c854041c7206a2c54
ludafieno <ludafieno@outlook.com>
2021-10-24 18:56:13 -0400

    BoardInspect code into ListView; add card issue

--- src/Pages/BoardListView.jsx
+++ src/Pages/BoardListView.jsx
@@ -3,7 +3,163 @@ import React from 'react'
 function BoardListView() {
     return (
         <div>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+          <section class="p-5">
+            <div class="container">
+              <div class="row text-center">
+                
+                <div class="col-md">
+                  <div class="card bg-light text-dark">
+                    <div class="card-body text-center">
+                      <div class="h1 mb-3">
+                        <i class="bi bi-globe"></i>
+                      </div>
+                      <h3 class="card-title mb-3">
+                        Quest 1
+                      </h3>
+                      <h5 class="card-title mb-3">
+                        Location: ABC
+                      </h5>
+                      <p class="card-text text-sm-start">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
+                      </p>
+                      <p>
+                        1:00-2:00
+                      </p>
+                      <a href="#" class="btn btn-success pull-left">See More...</a>
+                    </div>
+                  </div>
+                </div>
+                
+
+                
+                <div class="col-md">
+                  <div class="card bg-light text-dark">
+                    <div class="card-body text-center">
+                      <div class="h1 mb-3">
+                        <i class="bi bi-archive"></i>
+                      </div>
+                      <h3 class="card-title mb-3">
+                        Quest 2
+                      </h3>
+                      <h5 class="card-title mb-3">
+                        Location: ABC
+                      </h5>
+                      <p class="card-text text-sm-start">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
+                      </p>
+                      <p>
+                        1:00-2:00
+                      </p>
+                      <a href="#" class="btn btn-success">See More...</a>
+                    </div>
+                  </div>
+                </div>
+                
+
+                
+                <div class="col-md">
+                  <div class="card bg-light text-dark">
+                    <div class="card-body text-center">
+                      <div class="h1 mb-3">
+                        <i class="bi bi-paperclip"></i>
+                      </div>
+                      <h3 class="card-title mb-3">
+                        Quest 3
+                      </h3>
+                      <h5 class="card-title mb-3">
+                        Location: ABC
+                      </h5>
+                      <p class="card-text text-sm-start">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
+                      </p>
+                      <p>
+                        1:00-2:00
+                      </p>
+                      <a href="#" class="btn btn-success">See More...</a>
+                    </div>
+                  </div>
+                </div>
+                
+
+                
+                <div class="col-md">
+                  <div class="card bg-light text-dark">
+                    <div class="card-body text-center">
+                      <div class="h1 mb-3">
+                        <i class="bi bi-paint-bucket"></i>
+                      </div>
+                      <h3 class="card-title mb-3">
+                        Quest 4 
+                      </h3>
+                      <h5 class="card-title mb-3">
+                        Location: ABC
+                      </h5>
+                      <p class="card-text text-sm-start">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
+                      </p>
+                      <p>
+                        1:00-2:00
+                      </p>
+                      <a href="#" class="btn btn-success">See More...</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        <div/>
           
+
+
+
+  <section class="p-5">
+    <div class="container">
+      <div class="row text-center">
+
+        
+        <div class="col-md-3">
+          <div class="card bg-light text-dark">
+            <div class="card-body text-center">
+              <div class="h1 mb-3">
+                <i class="bi bi-calculator"></i>
+              </div>
+              <h3 class="card-title mb-3">
+                Quest 5
+              </h3>
+              <h5 class="card-title mb-3">
+                Location: ABC
+              </h5>
+              <p class="card-text text-sm-start">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
+              </p>
+              <p>
+                1:00-2:00
+              </p>
+              <a href="#" class="btn btn-success">See More...</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+      
+
+
+      <div class="col-md-3">
+        <div class="card bg-light text-dark">
+          <div class="card-body text-center">
+            <div class="h1 mb-3">
+              <i class="bi bi-plus-circle-dotted"></i>
+            </div>
+            <h3 class="card-title mb-3">
+              Create Quest
+            </h3>
+            <a href="#" class="btn btn-primary">Begin</a>
+          </div>
+        </div>
+       </div>
+
         </div>
 
     )
