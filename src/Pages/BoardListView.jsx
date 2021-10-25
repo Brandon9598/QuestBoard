4c7c2d3629ce5af312789368474dd4632a7b5c62
ludafieno <ludafieno@outlook.com>
2021-10-24 21:01:06 -0400

    card component in BoardListView

--- src/Pages/BoardListView.jsx
+++ src/Pages/BoardListView.jsx
@@ -1,165 +1,29 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap';
 
 function BoardListView() {
     return (
         <div>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
-          <section class="p-5">
-            <div class="container">
-              <div class="row text-center">
-                
-                <div class="col-md">
-                  <div class="card bg-light text-dark">
-                    <div class="card-body text-center">
-                      <div class="h1 mb-3">
-                        <i class="bi bi-globe"></i>
-                      </div>
-                      <h3 class="card-title mb-3">
-                        Quest 1
-                      </h3>
-                      <h5 class="card-title mb-3">
-                        Location: ABC
-                      </h5>
-                      <p class="card-text text-sm-start">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
-                      </p>
-                      <p>
-                        1:00-2:00
-                      </p>
-                      <a href="#" class="btn btn-success pull-left">See More...</a>
-                    </div>
-                  </div>
-                </div>
-                
 
-                
-                <div class="col-md">
-                  <div class="card bg-light text-dark">
-                    <div class="card-body text-center">
-                      <div class="h1 mb-3">
-                        <i class="bi bi-archive"></i>
-                      </div>
-                      <h3 class="card-title mb-3">
-                        Quest 2
-                      </h3>
-                      <h5 class="card-title mb-3">
-                        Location: ABC
-                      </h5>
-                      <p class="card-text text-sm-start">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
-                      </p>
-                      <p>
-                        1:00-2:00
-                      </p>
-                      <a href="#" class="btn btn-success">See More...</a>
-                    </div>
-                  </div>
-                </div>
-                
-
-                
-                <div class="col-md">
-                  <div class="card bg-light text-dark">
-                    <div class="card-body text-center">
-                      <div class="h1 mb-3">
-                        <i class="bi bi-paperclip"></i>
-                      </div>
-                      <h3 class="card-title mb-3">
-                        Quest 3
-                      </h3>
-                      <h5 class="card-title mb-3">
-                        Location: ABC
-                      </h5>
-                      <p class="card-text text-sm-start">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
-                      </p>
-                      <p>
-                        1:00-2:00
-                      </p>
-                      <a href="#" class="btn btn-success">See More...</a>
-                    </div>
-                  </div>
-                </div>
-                
-
-                
-                <div class="col-md">
-                  <div class="card bg-light text-dark">
-                    <div class="card-body text-center">
-                      <div class="h1 mb-3">
-                        <i class="bi bi-paint-bucket"></i>
-                      </div>
-                      <h3 class="card-title mb-3">
-                        Quest 4 
-                      </h3>
-                      <h5 class="card-title mb-3">
-                        Location: ABC
-                      </h5>
-                      <p class="card-text text-sm-start">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
-                      </p>
-                      <p>
-                        1:00-2:00
-                      </p>
-                      <a href="#" class="btn btn-success">See More...</a>
-                    </div>
-                  </div>
-                </div>
+          <section className="p-5">
+            <div className="container">
+              <div className="row text-cetner">
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>Location: ABC</Card.Text>
+                    <Card.Text>Time: 1:00 - 2:00 </Card.Text>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.  
+                    </Card.Text>
+                    <Button variant="primary">See more...</Button>
+                  </Card.Body>
+                </Card>
               </div>
             </div>
           </section>
-        <div/>
-          
-
-
-
-  <section class="p-5">
-    <div class="container">
-      <div class="row text-center">
-
-        
-        <div class="col-md-3">
-          <div class="card bg-light text-dark">
-            <div class="card-body text-center">
-              <div class="h1 mb-3">
-                <i class="bi bi-calculator"></i>
-              </div>
-              <h3 class="card-title mb-3">
-                Quest 5
-              </h3>
-              <h5 class="card-title mb-3">
-                Location: ABC
-              </h5>
-              <p class="card-text text-sm-start">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur culpa iure dolores voluptas aut! Accusantium?
-              </p>
-              <p>
-                1:00-2:00
-              </p>
-              <a href="#" class="btn btn-success">See More...</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-      
-
-
-      <div class="col-md-3">
-        <div class="card bg-light text-dark">
-          <div class="card-body text-center">
-            <div class="h1 mb-3">
-              <i class="bi bi-plus-circle-dotted"></i>
-            </div>
-            <h3 class="card-title mb-3">
-              Create Quest
-            </h3>
-            <a href="#" class="btn btn-primary">Begin</a>
-          </div>
-        </div>
-       </div>
-
         </div>
 
     )
